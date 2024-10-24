40a2faf38a9a906a8e2259463fb15f9f74b24c09
Divya <divvis888@gmail.com>
2024-10-23 23:25:57 -0700

    bfs problem - level order traversal of binary tree recursive

--- bfs-binarytree.py
+++ bfs-binarytree.py
@@ -2,12 +2,15 @@
 #Input: root = [1,2,3,4,5,6,7]
 
 #Output: [[1],[2,3],[4,5,6,7]]
+from typing import List, Optional
+from collections import deque 
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
-from typing import List, Optional
+
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         result = []
@@ -24,6 +27,27 @@ class Solution:
         bfs(root,0)
         return result
     
+
+    def levelOrder_iterative(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = []
+        if not root:
+            return []
+        visited = deque()
+        visited.append(root)
+        
+        while visited:
+            val = []
+            for i in range(len(visited)):
+                node = visited.popleft()
+                val.append(node.val)
+                if node.left:
+                    visited.append(node.left)
+                if node.right:
+                    visited.append(node.right)
+            result.append(val)
+        return result
+        
+        
 if __name__=="__main__":
     root = TreeNode(1)
     root.left = TreeNode(2)
@@ -32,4 +56,7 @@ if __name__=="__main__":
     root.left.right = TreeNode(5)
     root.right.right = TreeNode(6)
     solution = Solution()
+
     print(solution.levelOrder(root))
+    print(solution.levelOrder_iterative(root))
+
